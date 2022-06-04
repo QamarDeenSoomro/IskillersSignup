8687a10fb99b100ae2ec8759d7fb61fe602a2f8f
QamarDeenSoomro <qamersoomro1993@gmail.com>
2022-06-04 08:09:56 +0500

    first Commit

---
+++ app.js
@@ -0,0 +1,69 @@
+var FullName = document.getElementById("FullName");
+var FathersName = document.getElementById("FathersName");
+var email = document.getElementById("email");
+var upass = document.getElementById("upass");
+var CNIC = document.getElementById("CNIC");
+var MobileNo = document.getElementById("MobileNo");
+var courses = document.getElementById("courses");
+var qualification = document.getElementById("qualification");
+var gender = document.getElementById("gender");
+var laptop = document.getElementById("laptop");
+var message = document.getElementById("message");
+function signup() {
+    if (FullName.value === "") {
+        message.innerHTML = "Input your name"
+        FullName.focus();
+    } else if (FathersName.value === "") {
+        message.innerHTML = "Input your Father's Name"
+        FathersName.focus();
+    } else if (email.value === "") {
+        message.innerHTML = "Input Your email"
+        email.focus();
+    } else if (upass.value.length === 0) {
+        message.innerHTML = "Input Your Password"
+    } else if (upass.value.length < 7) {
+        message.innerHTML = "Password is not Strong Enough"
+    } else if (CNIC.value === "") {
+        message.innerHTML = "Input your CNIC"
+    } else if (CNIC.value.length != 13) {
+        message.innerHTML = "Incorrect CNIC format"
+    } else if (MobileNo.value === "") {
+        message.innerHTML = "Input Mobile Number"
+    } else if (MobileNo.value.length != 11) {
+        message.innerHTML = "Incorrect Mobile Number"
+    } else if (courses.value === "") {
+        message.innerHTML = "Select a Course"
+    } else if (qualification.value === "") {
+        message.innerHTML = "Choose your qualification"
+    } else if (gender.value === "") {
+        message.innerHTML = "Choose your Gender"
+    }
+    else {
+        // for (let i = 0; i < laptop.length; i++) {
+        //     if (laptop[i].cheked) {
+
+        //     }
+
+        // }
+        if (laptop.checked) {
+
+
+            var UserData = {
+                Name: FullName.value,
+                FName: FathersName.value,
+                Email: email.value,
+                UPass: upass.value,
+                CNIC: CNIC.value,
+                MobileNo: MobileNo.value,
+                courses: courses.value,
+                qualification: qualification.value,
+                gender: gender.value,
+                laptop: laptop.value,
+            }
+            message.innerHTML = "";
+            console.log(UserData);
+        } else {
+            message.innerHTML = "You Must have laptop"
+        }
+    }
+}
